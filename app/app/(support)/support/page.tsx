a33cfd9222baa7d70b14ad6f8d0f18da7ce43c40
Hr-kutsarov <hr.kutsarov89@gmail.com>
2024-11-21 21:51:54 +0200

    Project init

---
+++ app/app/(support)/support/page.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+
+export default function SupportPage() {
+    return (
+        <>
+        <main className={cn('w-full h-auto min-h-screen bg-zinc-100')}>
+            support page
+        </main>
+        </>
+    )
+}
